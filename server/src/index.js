7def6e041d7e6d36d8c5d99c4325cf4bd17b8f74
fmarincode <florent.marin59@gmail.com>
2023-11-02 18:09:17 +0100

    add dotenv

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 import express from "express"; // create a api mech
 import cors from "cors"; // allowed communication between front & back
 import mongoose from "mongoose" // for mongoDB 
-import bodyParser from "body-parser"
-
 import {imgsRouter} from './routes/images.js'
 import {adminRouter} from './routes/admin.js'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
 
@@ -19,8 +20,7 @@ app.use("/images", imgsRouter)
 app.use("/", adminRouter)
 
 
-mongoose.connect(
-    "mongodb+srv://Bascom3000:MeaculpaCHO17.@memoryapp.m8u9ftg.mongodb.net/memoryApp?retryWrites=true&w=majority"
-    )
+mongoose.connect(process.env.MONGODB_URI);
+
 
 app.listen(8000, () => console.log("SERVER STARTED!"))
