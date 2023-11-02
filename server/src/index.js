5521453f3d05b91108b6ad6d4fbdb1f38f277474
fmarincode <florent.marin59@gmail.com>
2023-11-02 17:44:47 +0100

    ..

---
+++ server/src/index.js
@@ -0,0 +1,25 @@
+import express from "express"; // create a api mech
+import cors from "cors"; // allowed communication between front & back
+import mongoose from "mongoose" // for mongoDB 
+import {imgsRouter} from './routes/images.js'
+import {adminRouter} from './routes/admin.js'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+
+app.use("/images", imgsRouter)
+app.use("/", adminRouter)
+
+
+mongoose.connect(process.env.MONGODB_URI);
+
+
+app.listen(8000, () => console.log("SERVER STARTED!"))
