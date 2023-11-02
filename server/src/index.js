0502f486ab6546cb56d910fbd4b3a717731a734a
fmarincode <florent.marin59@gmail.com>
2023-11-02 20:04:32 +0100

    hope lost

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,13 @@
 import express from "express"; // create a api mech
 import cors from "cors"; // allowed communication between front & back
 import mongoose from "mongoose" // for mongoDB 
-import bodyParser from "body-parser"
-
+import dotenv from 'dotenv';
 import {imgsRouter} from './routes/images.js'
 import {adminRouter} from './routes/admin.js'
 
 
 
+dotenv.config();
 const app = express()
 
 app.use(express.json())
@@ -18,9 +18,7 @@ app.use(cors())
 app.use("/images", imgsRouter)
 app.use("/", adminRouter)
 
+mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connect(
-    "mongodb+srv://Bascom3000:MeaculpaCHO17.@memoryapp.m8u9ftg.mongodb.net/memoryApp?retryWrites=true&w=majority"
-    )
 
 app.listen(8000, () => console.log("SERVER STARTED!"))
