fba98517c1658805da4b954aa9e753e2db2024a0
fmarincode <florent.marin59@gmail.com>
2023-11-14 19:25:45 +0100

    start step to push to render

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './Contexts/auth/AuthProvider';
+import {disableReactDevTools } from "@fvilers/disable-react-devtools"
+
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
