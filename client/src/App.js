a9cf2900e9f6b2693622d4de0813bcb261f3c714
fmarincode <florent.marin59@gmail.com>
2023-11-15 11:13:49 +0100

    add backend url from render

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
   const [message, setMessage] = useState("");
    // Fetching message from backend on mount
    useEffect(() => {
-    fetch("http://localhost:8000")
+    fetch("https://memorycardgame.onrender.com/")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
