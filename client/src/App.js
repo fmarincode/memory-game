6439b12afd906c70b17c132e78f669ac892601d4
fmarincode <florent.marin59@gmail.com>
2023-11-21 17:10:42 +0100

    delete 2 useless pages

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,10 @@ import Home from './pages/Home';
 import { ThemeProvider } from './Contexts/themeContext';
 import Footer from "./components/Footer"
 import Copyright from './pages/Copyright';
-import PostImg from './pages/PostImg';
-import PostTheme from './pages/PostTheme';
 import UserConnect from './pages/UserConnect';
 import RequireAuth from './components/RequireAuth';
-import "./App.css";
-import EditTheme from './pages/EditTheme';
 import Dashboard from './pages/Dashboard';
+import "./App.css";
 
 
 function App() {
@@ -30,17 +27,12 @@ function App() {
           <Navbar />
           <Routes>
          
-              {/* public routes */}
+
               <Route path='/' element={<Home />} />
               <Route path='/copyright' element={<Copyright />} />
               <Route path='/userconnect' element={<UserConnect />} />
 
-      
-  
-               {/* routes à protéger */}
-              <Route path="/addContent" element={<RequireAuth requiredRole={["Admin", "User"]}><PostImg /></RequireAuth>} />
-              <Route path="/addTheme" element={<RequireAuth requiredRole={["Admin", "User"]}><PostTheme /></RequireAuth>} />
-              <Route path="/editTheme" element={<RequireAuth requiredRole={["Admin", "User"]}><EditTheme /></RequireAuth>} />
+    
               <Route path="/dashboard" element={<RequireAuth requiredRole={["Admin", "User"]}><Dashboard /></RequireAuth>} />
               
         </Routes>
